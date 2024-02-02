aae4e9e3000a254886d01e0741e0ff0102645ae1
Clint Rutkas <clint@rutkas.com>
2024-02-01 22:27:30 -0800

    random cleanup

--- setup/boot.ps1
+++ setup/boot.ps1
@@ -1,11 +1,10 @@
 $mypath = $MyInvocation.MyCommand.Path
-Write-Output "Path of the script : $mypath"
+Write-Output "Path of the script: $mypath"
 Write-Output "Args for script: $Args"
 
 $isWinGetRecent = (winget -v).Trim('v').TrimEnd("-preview").split('.')
 if(!(($isWinGetRecent[0] -gt 1) -or ($isWinGetRecent[0] -ge 1 -and $isWinGetRecent[1] -ge 6))) # WinGet is greater than v1 or v1.6 or higher
 {
-   
    $paths = "Microsoft.VCLibs.x64.14.00.Desktop.appx", "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle", "Microsoft.UI.Xaml.2.7.x64.appx"
 
    Write-Information "Downloading WinGet and its dependencies..."
@@ -36,7 +35,7 @@ if(!(($isWinGetRecent[0] -gt 1) -or ($isWinGetRecent[0] -ge 1 -and $isWinGetRece
    }
 }
 else {
-   Write-Information "WinGet in decent state"
+   Write-Information "WinGet in decent state, moving to executing DSC"
 }
 
 $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
